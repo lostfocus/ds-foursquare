42583ee85b44ddcc200af2d164d25a1170d228a6
Dominik Schwind <dschwind@lostfocus.de>
2013-01-05 19:47:44 +0100

    Initial commit

---
+++ ds-foursquare.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: Foursquare WP
+Plugin URI: http://lostfocus.de/
+Version: 0.0.1
+Description: Imports Foursquare Checkins as blogposts.
+Author: Dominik Schwind
+Author URI: http://lostfocus.de/
+*/
